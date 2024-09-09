31e0a81396860d320c16d34194b906b14f95b48a
LimesKey <limeskeys@gmail.com>
2024-09-08 22:46:26 -0400

    website update (#1082)
    * website template
    
    * Add website changes
    
    Co-authored-by: Arthur Beck <averse.abfun@gmail.com>
    Co-authored-by: Aryan Kapoor <Aryankpoor@users.noreply.github.com>
    Co-authored-by: Max Wofford <max@maxwofford.com>
    
    * add 3d model with threatle
    
    * small website updates
    
    ---------
    
    Co-authored-by: Arthur Beck <averse.abfun@gmail.com>
    Co-authored-by: Aryan Kapoor <Aryankpoor@users.noreply.github.com>
    Co-authored-by: Max Wofford <max@maxwofford.com>

---
+++ showcase/src/lib/components/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import Scene from './Scene.svelte';
+
+</script>
+
+<div>
+	<Canvas>
+	  <Scene />
+	</Canvas>
+</div>
+  
+<style>
+	div {
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
+	}
+</style>
+
