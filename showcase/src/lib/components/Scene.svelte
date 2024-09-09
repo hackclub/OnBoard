12144dc2622ce25d60a42d55a83423b3fe4c01e3
LimesKey <limeskeys@gmail.com>
2024-09-08 22:46:26 -0400

    website update (#1082)
    * website template
    
    * Add website changes
    
    Co-authored-by: Arthur Beck <averse.abfun@gmail.com>
    Co-authored-by: Aryan Kapoor <Aryankpoor@users.noreply.github.com>
    Co-authored-by: Max Wofford <max@maxwofford.com>
    
    * add 3d model with threatle
    
    * small website updates
    
    ---------
    
    Co-authored-by: Arthur Beck <averse.abfun@gmail.com>
    Co-authored-by: Aryan Kapoor <Aryankpoor@users.noreply.github.com>
    Co-authored-by: Max Wofford <max@maxwofford.com>

---
+++ showcase/src/lib/components/Scene.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Model from './models/pcb.svelte';
+	import { T, useFrame} from '@threlte/core';
+	import { OrbitControls } from '@threlte/extras'
+
+	let rotation = 0;
+
+	useFrame((_, delta) => {
+		rotation += delta * 0.4
+	})
+	
+</script>
+
+<T.AmbientLight intensity={1} />
+
+<T.OrthographicCamera
+	zoom={20}
+	makeDefault
+	position={[0, 0, 100]}
+	<!-- on:create={({ ref }) => {
+		ref.lookAt(0, 1, 0);
+	}}> -->
+
+</T.OrthographicCamera>
+
+<Model scale={100} position.x={-15} position.y={8} position.z={0} />
