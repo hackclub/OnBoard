c5758ee813eba09384d1a619c2d7eea6e183f287
LimesKey <limeskeys@gmail.com>
2024-09-09 16:33:46 -0400

    fix cloudflare issue

--- showcase/vite.config.ts
+++ showcase/vite.config.ts
@@ -5,16 +5,9 @@ import * as path from "path";
 export default defineConfig({
 	plugins: [sveltekit()],
 
-	server: {
-		fs: {
-		  // Allow serving files from one level up to the project root
-		  allow: ['./static/electronics/'],
-		},
-	  },
-
 	resolve: {
     alias: {
-      "@assets": path.resolve(__dirname, "static"),
+      "@lib": path.resolve(__dirname, "src/lib"),
     },
   },
 
