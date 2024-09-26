c3ea0426be25ea8a7baf79e2bf842caa9b3360ee
LimesKey <limeskeys@gmail.com>
2024-09-26 13:50:51 -0400

    last fix for readme

--- showcase/vite.config.ts
+++ showcase/vite.config.ts
@@ -3,22 +3,11 @@ import { defineConfig, searchForWorkspaceRoot } from 'vite'
 import * as path from "path";
 
 export default defineConfig({
-  server: {
-    fs: {
-      allow: [
-        // search up for workspace root
-        searchForWorkspaceRoot(process.cwd()),
-        '/README.md',
-      ],
-    },
-  },
-
 	plugins: [sveltekit()],
 
 	resolve: {
     alias: {
       "@lib": path.resolve(__dirname, "src/lib"),
-      "/@": path.resolve(__dirname, "/"),
     },
   },
 
